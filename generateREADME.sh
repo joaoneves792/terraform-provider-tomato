073e8831692fd74898774d848f5caac4d77f69d4
Joao Miguel Neves <joaoneves792@gmail.com>
2022-11-07 16:15:52 +0000

    generic resource implementation

---
+++ generateREADME.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+cat > README.md << "EOT"
+# Terraform Provider Tomato
+This repo contains a provider for manipulating Tomato router settings through terraform.
+
+This was tested only on FreshTomato instalations on an ASUS RT-N12 and an ASUS RT-AC68U.
+
+To use this change `OS_ARCH=linux_amd64` to your architecture and run `make` (alternatively run `make release` to build for all architectures)
+
+In your terraform code include the provider as:
+```
+terraform {
+  required_providers {
+    tomato = {
+      version = ">=0.0.3"
+      source  = "warpenguin.dev/tomato/tomato"
+    }
+  }
+}
+
+provider "tomato" {
+  url = "https://10.0.0.1"
+  #Provide username and password with TOMATO_USERNAME and TOMATO_PASSWORD env variables, or alternatively set them here
+  username = "xxxxx" 
+  password = "xxxxx"
+}
+
+```
+
+This provider exposes the following datasources and resources (see examples in ./examples/main.tf):
+
+
+EOT
+
+cat $(find ./docs/ -name '*.md') >> ./README.md
